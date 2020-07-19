852b3b57bf3db5485f4bcb653ab9f45e60aaa904
MatrixcoderBB <68490651+MatrixcoderBB@users.noreply.github.com>
2020-07-19 10:40:01 +0530

    Create JiljilapiApplicationTests.java

---
+++ src/test/java/com/api/jiljilapi/JiljilapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.jiljilapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiljilapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
