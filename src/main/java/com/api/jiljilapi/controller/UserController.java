849f4845865ea502763e8ea9a65c4051a26fe9c6
MatrixcoderBB <68490651+MatrixcoderBB@users.noreply.github.com>
2020-07-19 10:40:11 +0530

    Create UserController.java

---
+++ src/main/java/com/api/jiljilapi/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.api.jiljilapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@RequestMapping(value="/signin")
+	public String signin() {
+		return "Successfully signed in!!!!";
+	}
+}
